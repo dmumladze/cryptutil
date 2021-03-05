730d932447f787f7ebf89847e4291b74ba925dc1
m0bydk <avelchikviladze@gmail.com>
2021-03-04 23:25:41 -0500

    added validations

--- src/main/java/com/citigroup/icg/cmc/ArchiveOptions.java
+++ src/main/java/com/citigroup/icg/cmc/ArchiveOptions.java
@@ -2,12 +2,17 @@ package com.citigroup.icg.cmc;
 
 import org.apache.commons.cli.*;
 
+import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class ArchiveOptions {
@@ -28,6 +33,7 @@ public class ArchiveOptions {
         requiredOptions.addRequiredOption("I", "input-path", true, "Directory or list of file to encrypt. Invalid files will be skipped without warning.");
         requiredOptions.addRequiredOption("P", "password", true, "Password for encrypted files.");
         requiredOptions.addOption("S", "skip-ext", true, "Skips files with provided extensions.");
+        requiredOptions.addOption("D", "skip-older-than", true, "Skips files older than X days.");
         requiredOptions.addOption("T", "test", false, "Test run will neither encrypt nor deletes files, but will generate the full report.");
         requiredOptions.addOption("O", "output-file", true, "File to print the report to. If the file exists, it will be recreated with each run.");
         requiredOptions.addOption("H", "help", false, "Displays help for command-line options.");
@@ -56,8 +62,48 @@ public class ArchiveOptions {
 
     public ValidationResults validate() {
         ValidationResults results = new ValidationResults();
-        //TODO: validate all input options
+
+        Pattern upper = Pattern.compile("[A-Z]");
+        Pattern lower = Pattern.compile("[a-z]");
+        Pattern numbers = Pattern.compile("[0-9]");
+        Pattern alphaNumeric = Pattern.compile("[^a-zA-Z0-9]");
+
+        if (Files.notExists(inputPath))
+            results.addInputPathError("Input path does not exist.");
+
+        if (password.length() < 6)
+            results.addPasswordError("The password contains less than 6 chars.");
+        if (!textMatches(upper, password))
+            results.addPasswordError("The password does not contain upper case letters.");
+        if (!textMatches(lower, password))
+            results.addPasswordError("The password does not contain lower case letters.");
+        if (!textMatches(numbers, password))
+            results.addPasswordError("The password does not contain numbers.");
+        if (!textMatches(alphaNumeric, password))
+            results.addPasswordError("The password does not contain special characters.");
+
+        for (String ext : skipExt) {
+            if (textMatches(alphaNumeric, ext)) {
+                results.addSkipExtError(String.format("'%s' contains special chars.", ext));
+            }
+        }
+
+        if (outputFilePath != null) {
+            try {
+                Files.createFile(outputFilePath);
+            } catch (Exception e) {
+                results.addOutputFilePathError("Output file path is invalid.");
+            }
+        }
+
+        //TODO: --skip-older-than argument should not be less then 0
+
         return results;
+    }
+
+    private boolean textMatches(Pattern pattern, String text) {
+        Matcher m = pattern.matcher(text);
+        return m.find();
     }
 
     public Path getInputPath() {
--- src/main/java/com/citigroup/icg/cmc/ValidationResultsPrinter.java
+++ src/main/java/com/citigroup/icg/cmc/ValidationResultsPrinter.java
@@ -1,23 +1,37 @@
 package com.citigroup.icg.cmc;
 
+import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
+
 import java.util.Collection;
+
 
 public class ValidationResultsPrinter implements Visitor<ValidationResults> {
 
     @Override
     public void visit(ValidationResults results) {
-        /*
-            TODO: System.out.println all validation error
-            Validation errors:
-            input-path
-                - does not exist
-            password
-                - is not strong
-                - is missing
-        */
-        Collection<String> intputPathErrors = results.getInputPathErrors();
-        if (!intputPathErrors.isEmpty()) {
-            //print error here
+        Collection<String> inputPathErrors = results.getInputPathErrors();
+        if (!inputPathErrors.isEmpty()) {
+            System.out.println("--input-path");
+            //inputPathErrors.forEach(System.out::println);
+            inputPathErrors.forEach(e -> System.out.println("\t" + e));
+        }
+
+        Collection<String> passwordError = results.getPasswordErrors();
+        if (!passwordError.isEmpty()) {
+            System.out.println("--password");
+            passwordError.forEach(e -> System.out.println("\t" + e));
+        }
+
+        Collection<String> skipExt = results.getSkipExtErrors();
+        if (!skipExt.isEmpty()) {
+            System.out.println("--skip-ext");
+            skipExt.forEach(e -> System.out.println("\t" + e));
+        }
+
+        Collection<String> outputFilePathErrors = results.getOutputFilePathErrors();
+        if (!outputFilePathErrors.isEmpty()) {
+            System.out.println("--output-file");
+            outputFilePathErrors.forEach(e -> System.out.println("\t" + e));
         }
     }
 }
